2a42c992fae8052e97017a95b6b106cd2b6a5c0c
yorick1125 <60619352+yorick1125@users.noreply.github.com>
2022-10-04 14:16:45 -0400

    progress on samurai

--- SamuraiApp/SamuraiApp.UI/Program.cs
+++ SamuraiApp/SamuraiApp.UI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using static System.Console;
@@ -14,13 +15,16 @@ namespace SamuraiApp.UI
 
         private static void Main(string[] args)
         {
-            AddSamurai("Claudiu Sensei", "Aref Sensei", "Sandy Sensei", "Gabriel Sensei");
+            AddSamurais("Claudiu Sensei", "Aref Sensei", "Sandy Sensei",
+                        "Gabriel Sensei", "Larry Sensei", "Ian Sensei",
+                        "Helen Sensei", " Vik Sensei", "Chris Sensei");
+
 
             //AddSamurais("Shimada", "Okamoto", "Kikuchio", "Hayashida");
-            GetSamurais();
+            //GetSamurais();
             //AddVariousTypes();
             //QueryFilters();
-            //QueryAggregates();
+            QueryAggregates();
             //RetrieveAndUpdateSamurai();
             //RetrieveAndUpdateMultipleSamurais();
             //MultipleDatabaseOperations();
@@ -33,7 +37,7 @@ namespace SamuraiApp.UI
 
 
            
-        private static void AddSamurai(params string[] names)
+        private static void AddSamurais(params string[] names)
         {
             foreach (string name in names)
             {
@@ -84,33 +88,78 @@ namespace SamuraiApp.UI
 
         private static void QueryFilters()
         {
-            // 1. 
+            // 1 .
             var name = "C%";
+            var samurais = _context.Samurais.Where(s => s.Name == name).ToList();
+            // 2 .
+            var filter = "C%";
+            var samurais2 = _context.Samurais
+                 .Where(s => EF.Functions.Like(s.Name, filter)).ToList();
+            // 3 .
+            // This code is equivalent to EF . Functions . Like
+            // except that this searches for % C %
+            var samurais3 = _context.Samurais
+                 .Where(s => s.Name.Contains("C")).ToList();
+            Print(samurais, "1. ");         // q : why is this samurai not shown ?
+            Print(samurais2, "2. ");
+            Print(samurais3, "3. ");
+            
         }
 
         private static void QueryAggregates()
         {
+            var name = "Claudiu Sensei";
+            var samurai = _context.Samurais.FirstOrDefault(s => s.Name == name);
 
+            int id = 5; // the PK id might need to be different for your scenario
+            var samurai2 = _context.Samurais.Find(id);
+
+            Print(samurai);
+            Print(samurai2);
         }
 
         private static void RetrieveAndUpdateSamurai()
         {
+            var samurai = _context.Samurais.FirstOrDefault();
+            samurai.Name += " San ";
 
+            _context.SaveChanges();
+            Print(samurai);
         }
 
-        private static void RetrieveAndUpdateMultipleSamurai()
+        private static void RetrieveAndUpdateMultipleSamurais()
         {
-
+            var samurais = _context.Samurais.Skip(1).Take(4).ToList();
+            samurais.ForEach(s => s.Name += " San " ) ;
+            _context.SaveChanges() ;
+            Print(samurais );
         }
 
         private static void MultipleDatabaseOperations()
         {
-
+             var samurai = _context.Samurais.FirstOrDefault();
+            samurai.Name += " San " ;
+            _context.Samurais.Add( new Samurai { Name = " Shino " } ) ;
+            _context.SaveChanges( ) ;
+            Print(samurai );
         }
 
         private static void RetrieveAndDeleteASamurai()
         {
+            int id = 6;
+            var samurai = _context.Samurais.Find(id);
 
+            if(samurai != null)
+            {
+                _context.Samurais.Remove(samurai);
+                _context.SaveChanges();
+
+                Print(samurai);
+            }
+            else
+            {
+                WriteLine($"Samurai with {id} not found");
+            }
         }
 
 
