3b8ec914bff41f42e24027bf0aad21b4094aecde
yorick1125 <60619352+yorick1125@users.noreply.github.com>
2022-10-17 14:01:34 -0400

    started milestone 1

---
+++ Milestone1/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlSever(
+                @"Data Source= (localdb)\MSSQLLocalDB; Initial Catalog=SamuraiDB-Niyonkuru-Yorick;");
+        }
+    }
+
+
+}
