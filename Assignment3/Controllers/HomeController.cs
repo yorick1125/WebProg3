b24c3626283ecfaf935aabc0d056a73dbee1e124
yorick1125 <60619352+yorick1125@users.noreply.github.com>
2022-11-12 11:54:53 -0500

    began assignment3

---
+++ Assignment3/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Assignment3.Models;
+using DutchTreat.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactModel contact)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Success", contact);
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assignment3/Models/ContactModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DutchTreat.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "First Name must have at least 2 characters long. ")]
+        [RegularExpression("^[^0-9]+$", ErrorMessage = "First Name cannot contain numbers. ")]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Last Name must have at least 2 characters long. ")]
+        [RegularExpression("^[^0-9]+$", ErrorMessage = "Last Name cannot contain numbers. ")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@".*@.*\.\w{2,}", ErrorMessage = "Please enter a valid email address. ")]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone. ")]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Topic { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Message { get; set; }
+
+
+    }
+}
